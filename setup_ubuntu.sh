b69d9fbd783000c01af9cfef5c0b4b9edeb6622e
Amrita Nischal <nischal.a@northeastern.edu>
2024-10-03 00:19:38 -0400

    Add ubuntu script

---
+++ setup_ubuntu.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+# Install zip and unzip
+sudo apt install -y zip unzip
+
+# Navigate to tmp folder
+cd /tmp || exit
+
+# Install Java
+wget -O - https://apt.corretto.aws/corretto.key | sudo gpg --dearmor -o /usr/share/keyrings/corretto-keyring.gpg && \
+echo "deb [signed-by=/usr/share/keyrings/corretto-keyring.gpg] https://apt.corretto.aws stable main" | sudo tee /etc/apt/sources.list.d/corretto.list
+
+sudo apt update -y; sudo apt install -y java-21-amazon-corretto-jdk
+
+# Verify Java
+java -version
+
+# Install Gradle
+apt install gradle
+
+# Update ubuntu
+apt update -y
+
+# Install PostgreSQL
+apt install -y postgresql postgresql-contrib
+
+# Restart PostgreSQL
+sudo systemctl restart postgresql.service
+
+printf "\nEnter postgres user 'csye' password:\n\n"
+sudo -u postgres createuser csye -P --superuser
+
+export JAVA_HOME=$(readlink -f `which javac` | sed "s:/bin/javac::")
+export PATH="${PATH}":"${JAVA_HOME}"/bin
+
+# Create database
+sudo -u postgres psql -c 'create database app_db;'
+
+# Open port 8080
+ufw allow 8080
