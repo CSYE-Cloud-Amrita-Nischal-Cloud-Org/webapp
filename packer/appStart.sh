5c928f896399418a743469001cbafea824f5b85a
Amrita Nischal <nischal.a@northeastern.edu>
2024-10-23 03:25:59 -0400

    Update db url and shell scripts for rds

--- packer/appStart.sh
+++ packer/appStart.sh
@@ -3,7 +3,4 @@
 export JAVA_HOME=/usr/lib/jvm/java-21-amazon-corretto
 export PATH=$PATH:$JAVA_HOME/bin
 
-# shellcheck source=/dev/null
-source /opt/webapp/db_creds.sh
-
-java -Ddb.username="$DB_USERNAME" -Ddb.password="$DB_PASSWORD" -jar /opt/webapp/app.jar
+java -Ddb.username="$DB_USERNAME" -Ddb.password="$DB_PASSWORD" -Ddb.url="$DB_URL" -jar /opt/webapp/app.jar
